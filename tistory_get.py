b903171e2e8444b30be18967ef42e7c5a71fadfa
taeho9 <unicenter@naver.com>
2023-07-23 07:35:05 +0000

    P 태그 로직 정리 직전

--- tistory_get.py
+++ tistory_get.py
@@ -34,11 +34,11 @@ try:
 			post.raise_for_status()
 		except requests.exceptions.HTTPError as e:
 			if post.status_code == 404:
-				print('Post를 찾을 수 없습니다.' + str(index) + ' - 404 Not Found')
+				print('Post를 찾을 수 없습니다. https://' + base_url + "/" + str(index) + ' - 404 Not Found')
 				time.sleep(3)
 				continue
 			else:
-				print('Post를 찾았습니다. Post 번호 : ' + str(index))
+				print('알 수 없는 예러가 발새앴습니다. https://' + base_urlr + "/" + str(index))
 
 		page = BeautifulSoup(post.content, "html.parser")
 
@@ -56,6 +56,11 @@ try:
 		wdate = page.find('li', class_='jb-article-information-date')
 		print('Post No : ' + str(index) + ', Title : ' + title.text)
 		print('Category : ' + category.text + ', Date : ' + wdate.text.strip())
+		print('WP Slug : /' + str(index))
+
+		# 다운로드 이미지 파일과 새롭게 생성한 contents에 추가한 <img> 태그의 카운트를 비교하기 위한 변수 초기화
+		img_file_count = 0
+		img_tag_count = 0
 
 		# Get Article
 		article = page.find('div', class_='contents_style')
@@ -114,6 +119,7 @@ try:
     		# 이미지 파일 저장. 파일명에 ? 가 포함되어 있을경우 파일저장시 파일명이 '로 묶여 저장되는 경우가 있음
 			with open(img_path.replace("?", "_"), 'wb') as img_file:
 				img_file.write(img_response.content)
+				img_file_count = img_file_count + 1     # 이미지 다운로드에 성공하면 카운터 1 증가
 				print(f"이미지 다운로드 완료: {fname}")
 			
 			# img 태그에서 src attribute의 값을 변경
@@ -132,21 +138,84 @@ try:
 		img_alt = ""
 
 		# 포스트 본문 추출. p 태그가 있는지 여부 판단함
-		for tag in article.find_all():
+		for tag in article.children:
 			if tag.name == "p":
 				# p tag 다음에 figure 태그가 있으면 이미지의 alt 태그로 처리함
 				sec_tag = tag.find('figure')
-				if sec_tag:  
+				if sec_tag and sec_tag.get('class') == ['imageblock', 'alignCenter']:
+					print("P태그 내부의 figure 찾음")
+					sec_tag = sec_tag.find('img')
+					alt_text = image.get('alt', '')
+					contents = contents + "<img src=" + '"/tistory/' + str(index) + "/" + os.path.basename(sec_tag['src'].replace("?", "_")) + '" alt="' + alt_text + '">\n'
+					img_tag_count = img_tag_count + 1
 					img_alt = sec_tag.text.strip()
-				else:  # 없으면 그냥 컨텐츠 문장
-					# <br> 태그와 텍스트를 추출하여 리스트로 저장
-					img_alt = ""
-				if tag.parent.get('class') == ['contents_style']:
-					contents = contents + "<p>" + tag.text.strip() + "</p>\n"
+				else:  # figure 없으면 혹시 imageblock span이 있는지 확인
+					sec_tags = tag.find_all('span')
+					for sec_tag in sec_tags:
+						if sec_tag and sec_tag.get('class') == ['imageblock']:
+							print("P태그 내부의 imageblock span 찾음")
+							image = sec_tag.find('img')
+							alt_text = image.get('alt', '')
+							contents = contents + "<img src=" + '"/tistory/' + str(index) + "/" + os.path.basename(image['src'].replace("?", "_")) + '" alt="' + alt_text + '">\n'
+							img_tag_count = img_tag_count + 1
+						else: # imageblock span이 없으면 imageblock div 태그가 있는지 확인
+							sec_tag = tag.find('div')
+							if sec_tag and (sec_tag.get('class') == ['imageblock','center'] or sec_tag.get('class') == ['imageblock']):
+								print("P태그 내부의 imageblock center 또는 imageblock div 찾음")
+								image = sec_tag.find('img')
+								alt_text = image.get('alt', '')
+								contents = contents + "<img src=" + '"/tistory/' + str(index) + "/" + os.path.basename(image['src'].replace("?", "_")) + '" alt="' + alt_text + '">\n'
+								img_tag_count = img_tag_count + 1
+							else:
+								img_alt = ""
+							# 이미지 블록 찾기 끝
+				# p 태그 다음에 iframe이 오는 유튜브 등 동영상 삽입한 iframe 찾기
+				sec_tag = tag.find('iframe')
+				if sec_tag:
+					contents = contents + sec_tag.prettiy()
+				# p 태그 내부에 table 태그가 있는지 찾기
+				sec_tag = tag.find('table')
+				if sec_tag:
+					print("P 태그 내부의 Table 태그를 찾음")
+					contents = contents + "<p>" + tag.prettify() + "</p>\n"
+					sec_tag.decompose()  # table tag 제거
+				# P 태그 내부에 <a> 태그 찾기
+				sec_tag = tag.find('a')
+				if sec_tag:
+					contents = contents + "<a href='" + sec_tag['href'] + "'>"+ sec_tag.text.strip() + "</a>"
+				# P 태그 내의 남아 있는 텍스트 추출하여 뒤에 추가
+				contents = contents + "<p>" + tag.text.strip() + "</p>\n"
+			elif tag.name == 'a':
+				contents = contents + "<a href='" + tag['href'] + "'>" + tag.text.strip() + "</a>"
+			elif tag.name == 'span':
+				if tag.get('class') == ['imageblock']:
+					print("P태그 없는 imageblock span 찾음")
+					img_tag_count = img_tag_count + 1
+					image = tag.find('img')
+					alt_text = image.get('alt', '')
+					contents = contents + "<img src=" + '"/tistory/' + str(index) + "/" + os.path.basename(image['src'].replace("?", "_")) + '" alt="' + alt_text + '">\n'
 			elif tag.name == "img":
+				print("그냥 img 태그 찾음")
+				img_tag_count = img_tag_count + 1
 				# 파일명에 ?가 있을경우 경우에 따라 파일저장 시 _로 대체되기 때문에 처리함. 앞에서 이미지파일 저장시에도 파일명에 ?가 있으면 _로 대체하여 저장함
 				contents = contents + "<img src=" + '"/tistory/' + str(index) + "/" + os.path.basename(tag['src'].replace("?", "_")) + '" alt="' + img_alt + '">\n'
+			elif tag.name == "strong":
+				contents = contents + "<p>" + tag.text.strip() + "</p>\n" 
 			elif tag.name == "div":
+				if tag.get('class') ==  ['imageblock', 'center'] or tag.get('class') == ['imageblock']:
+						print("div 태그 내부의 imageblock center 찾음")
+						img_tag_count = img_tag_count + 1
+						image = tag.find('img')
+						alt_text = image.get('alt', '')
+						contents = contents + "<img src=" + '"/tistory/' + str(index) + "/" + os.path.basename(image['src'].replace("?", "_")) + '" alt="' + alt_text + '">\n' 
+				else:
+					sec_tag = tag.find('span')
+					if sec_tag and sec_tag.get('class') == ['imageblock']:
+						print("div 태그 내부의 span 태그 내부의 imageblock 찾음")
+						img_tag_count = img_tag_count + 1
+						image = tag.find('img')
+						alt_text = image.get('alt', '')
+						contents = contents + "<img src=" + '"/tistory/' + str(index) + "/" + os.path.basename(image['src'].replace("?", "_")) + '" alt="' + alt_text + '">\n'
 				contents = contents + "<p>" + tag.text.strip() + "</p>\n" 
 			elif tag.name == "h2":
 				contents = contents + "<h2>" + tag.text.strip() + "</h2>\n"
@@ -174,20 +243,34 @@ try:
 				contents = contents + "</ul>\n"
 			elif tag.name == "span" and tag.parent.get('class') == ['contents_style']:
 				contents = contents + "<p>" + tag.text.strip() + "</p>\n"
+			elif tag.name == "table":
+				print("최상위의 Table 태그를 찾음")
+				contents = contents + "<p>" + tag.prettify() + "</p>\n"
+			elif tag.name == "font":
+				contents = contents + "<p>" + tag.text.strip() + "</p>\n"
+			elif isinstance(tag, str):  # 텍스트 노드인 경우
+				contents = contents + "<p>" + tag.text.strip() + "</p>\n" 
 
 		# 포스트를 저장할 html 파일명 생성
 		wfilename = os.getcwd() + "/tistory/" + str(index) + ".html"
 
 		# 포스트의 제목 등과 컨텐츠를 파일에 작성
 		with open(wfilename, "w") as file:
-			file.write("포스트 제목 : " + title.text.strip() + "\n")
+			file.write("포스트 제목     : " + title.text.strip() + "\n")
 			file.write("포스트 작성일자 : " + wdate.text.strip() + "\n")
-			file.write("카테고리 : " + category.text.strip() + "\n")
+			file.write("카테고리        : " + category.text.strip() + "\n")
+			file.write('워드프레스 Slug : /' + str(index) + "\n")
+			file.write("-------------\n")
+			file.write("다운로드 받은 이미지 개수 : " + str(img_file_count) + "\n")
+			file.write("생성한 IMG 태그 개수      : " + str(img_tag_count) + "\n")
 			file.write("-------------\n")
 			file.write(contents)
+		file.close()
 
+		print("다운로드 받은 이미지 개수 : " + str(img_file_count))
+		print("생성한 IMG 태그 개수      : " + str(img_tag_count))
 		time.sleep(3)
-
+	
 except KeyboardInterrupt:
     sys.exit(0)
 
